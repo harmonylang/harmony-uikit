ca578fcbc38705f2ba61a88a3ef90f7aa3bd7498
Anthony Yang <ay339@cornell.edu>
2021-02-14 18:22:38 -0500

    Add tests

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import parseCharmony from "./charmony/CharmonyData";
+export {CharmonyTopLevel, CharmonyExecutedCode} from "./charmony/CharmonyData"
+export {IntermediateJson} from './charmony/IntermediateJson';
 
-export * as charmonyTypes from './charmony/IntermediateJson';
 export const parse = parseCharmony;
